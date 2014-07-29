f7fdcce9f371205c028ae3eab23778cffbee2df3
Linooos <linuskirkwood@gmail.com>
2014-07-29 16:57:29 +0100

    Partial implementation of read function.

--- region.py
+++ region.py
@@ -1,21 +1,21 @@
 class Region:
-    llx = 0
-    lly = 0
+    xllc = 0
+    yllc = 0
     nrows = 3
     ncols = 3
     step = 50
     grid = [[1,2,3], [4,5,6], [3,8,9]]
 
     def read (self, filename):
+        file = open(filename,'r')
+        x = file.readline().split()
+        self.ncols = int(x[1])
             # set the variables from a file
-            pass
+            
 
 
 thisone = Region()
 
-print thisone.nrows
-thatone = Region()
-thatone.read("myfile.txt")
-print   thisone.grid[0]
-print   thisone.grid[1]
-print   thisone.grid[2]
+thisone.read("ascii grid 5/SX99SW.asc")
+
+print(thisone.ncols)
