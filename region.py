14a2ea74809de8db651b2342f0ea8e8bce659bc6
Linooos <linuskirkwood@gmail.com>
2014-07-30 13:14:33 +0100

    Changed "grid" member to be of type numpy array and provided a read function which takes a pathname (possibly relative) to an ascii grid file

--- region.py
+++ region.py
@@ -1,21 +1,32 @@
+from numpy import *
 class Region:
-    xllc = 0
-    yllc = 0
-    nrows = 3
-    ncols = 3
+    xllc = 0  #<--x
+    yllc = 0  #<--y
+    nrows = 3  #<-- b
+    ncols = 3  #<-- a
     step = 50
-    grid = [[1,2,3], [4,5,6], [3,8,9]]
-
+    grid = [[1,2,3], [4,5,6], [3,8,9] ]
     def read (self, filename):
         file = open(filename,'r')
+        a = file.readline().split()
+        self.ncols = int(a[1])
+        b = file.readline().split()
+        self.nrows = int(b[1])
         x = file.readline().split()
-        self.ncols = int(x[1])
-            # set the variables from a file
-            
+        self.xllc = int(x[1])
+        y = file.readline().split()
+        self.yllc = int(y[1])
+        z = file.readline().split()
+        self.step = int(z[1])
+
+        file.close
+        self.grid = loadtxt(filename, skiprows=5)
 
 
 thisone = Region()
 
 thisone.read("ascii grid 5/SX99SW.asc")
 
-print(thisone.ncols)
+print thisone.grid.size
+print thisone.grid[0][0]
+
