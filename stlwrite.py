8cba8c3f68e343c2e5c729f40ce5b64dc2043996
ChocolateBubbles <ferdiamckeogh@icloud.com>
2014-07-30 20:06:37 +0100

    Rewrote stlwrite.py to use thearn's numpty2stl source.

--- stlwrite.py
+++ stlwrite.py
@@ -1,98 +1,17 @@
 #Written by ChocolateBubbles, 2014
 #I strongly believe the .whatever format should be universal...
-import sys
+
 import region
 
-current = region.Region()
+from pylab import imread
+from scipy.ndimage import gaussian_filter
+from stl_tools import numpy2stl
 
-def write():
-    print "Generating STL file from map data..."
+r = region.Region()
+r.readgr ("SN400200")
 
-    name = 'MAPDATA.stl'
+print "Generating STL file from map data..."
 
-    try:
-            with open('MAPDATA.stl', 'a') as f2:               
-                f2.write("solid model" + '\n' + ("facet normal 0.0 0.0 0.0" + '\n' "outer loop" + '\n' +
-                                                     "vertex 100.0 0.0 0.0" + '\n' +
-                                                     "vertex 0.0 0.0 0.0" + '\n' +
-                                                     "vertex 0.0 100.0 0.0" + '\n' +
-                                                 "endloop" + '\n' + "endfacet") + '\n' +
-                         
-                                                ("facet normal 0.0 0.0 0.0" + '\n' "outer loop" + '\n' +
-                                                     "vertex 100.0 0.0 0.0" + '\n' +
-                                                     "vertex 100.0 100.0 0.0" + '\n' +
-                                                     "vertex 0.0 100.0 0.0" + '\n' +
-                                                 "endloop" + '\n' + "endfacet") + '\n' +
-                        
-                                                #^ This is for the two triangles of the bottom plane.
+numpy2stl(r.grid,"GENERATED.stl", solid=True)
 
-                                                
-                                                ("facet normal 0.0 0.0 0.0" + '\n' "outer loop" + '\n' +
-                                                     "vertex 0.0 0.0 0.0" + '\n' +
-                                                     "vertex 0.0 100.0 0.0" + '\n' +
-                                                     "vertex 0.0 0.0 5.0" + '\n' +
-                                                 "endloop" + '\n' + "endfacet") + '\n' +
-                         
-                                                ("facet normal 0.0 0.0 0.0" + '\n' "outer loop" + '\n' +
-                                                     "vertex 0.0 0.0 5.0" + '\n' +
-                                                     "vertex 0.0 100.0 0.0" + '\n' +
-                                                     "vertex 0.0 100.0 5.0" + '\n' +
-                                                 "endloop" + '\n' + "endfacet") + '\n' +
-                         
-                                                ("facet normal 0.0 0.0 0.0" + '\n' "outer loop" + '\n' +
-                                                     "vertex 0.0 0.0 0.0" + '\n' +
-                                                     "vertex 100.0 0.0 0.0" + '\n' +
-                                                     "vertex 0.0 0.0 5.0" + '\n' +
-                                                 "endloop" + '\n' + "endfacet") + '\n' +
-                         
-                                                ("facet normal 0.0 0.0 0.0" + '\n' "outer loop" + '\n' +
-                                                     "vertex 0.0 0.0 5.0" + '\n' +
-                                                     "vertex 100.0 0.0 0.0" + '\n' +
-                                                     "vertex 100.0 0.0 5.0" + '\n' +
-                                                 "endloop" + '\n' + "endfacet") + '\n' +
-                         
-                                                ("facet normal 0.0 0.0 0.0" + '\n' "outer loop" + '\n' +
-                                                     "vertex 100.0 100.0 5.0" + '\n' +
-                                                     "vertex 100.0 100.0 0.0" + '\n' +
-                                                     "vertex 100.0 0.0 0.0" + '\n' +
-                                                 "endloop" + '\n' + "endfacet") + '\n' +
-                         
-                                                ("facet normal 0.0 0.0 0.0" + '\n' "outer loop" + '\n' +
-                                                     "vertex 100.0 100.0 5.0" + '\n' +
-                                                     "vertex 100.0 0.0 0.0" + '\n' +
-                                                     "vertex 100.0 0.0 5.0" + '\n' +
-                                                 "endloop" + '\n' + "endfacet") + '\n' +
-
-                                                ("facet normal 0.0 0.0 0.0" + '\n' "outer loop" + '\n' +
-                                                     "vertex 0.0 100.0 5.0" + '\n' +
-                                                     "vertex 0.0 100.0 0.0" + '\n' +
-                                                     "vertex 100.0 100.0 0.0" + '\n' +
-                                                 "endloop" + '\n' + "endfacet") + '\n' +
-                         
-                                                ("facet normal 0.0 0.0 0.0" + '\n' "outer loop" + '\n' +
-                                                     "vertex 100.0 100.0 5.0" + '\n' +
-                                                     "vertex 100.0 100.0 0.0" + '\n' +
-                                                     "vertex 0.0 100.0 5.0" + '\n' +
-                                                 "endloop" + '\n' + "endfacet") + '\n' +
-
-                                                #^ This is for the 8 triangles on the 4 sides.
-
-
-                                                #Insert map generation code here
-                                                ("facet normal 0.0 0.0 0.0" + '\n' "outer loop" + '\n' +
-                                                     "vertex x1 z1 y1" + '\n' +
-                                                     "vertex  x2 z2 y2" + '\n' +
-                                                     "vertex x3 z3 y3" + '\n' +
-                                                 "endloop" + '\n' + "endfacet") + '\n' +
-                         
-                                                "endsolidmodel")
-                
-
-    except:
-        print('File creation error. Closing Python.')
-        sys.exit(0)
-print current.ncols
-#write()
-
-
-print "Done!"
+print "Done! GENERATED.stl is now ready to print!"
