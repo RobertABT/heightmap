ae39ee127e1126c1f3771dd1e7872eed7807afb6
Ferdia McKeogh <ferdiamckeogh@Ferdias-MacBook.local>
2014-07-30 15:24:56 +0100

    Moved region.py for testing.

--- STL Generation/stlwrite.py
+++ STL Generation/stlwrite.py
@@ -1,6 +1,9 @@
 #Written by ChocolateBubbles, 2014
 #I strongly believe the .whatever format should be universal...
 import sys
+import region
+
+current = region.Region()
 
 def write():
     print "Generating STL file from map data..."
@@ -72,17 +75,24 @@ def write():
                                                      "vertex 0.0 100.0 5.0" + '\n' +
                                                  "endloop" + '\n' + "endfacet") + '\n' +
 
-
                                                 #^ This is for the 8 triangles on the 4 sides.
+
+
                                                 #Insert map generation code here
+                                                ("facet normal 0.0 0.0 0.0" + '\n' "outer loop" + '\n' +
+                                                     "vertex x1 z1 y1" + '\n' +
+                                                     "vertex  x2 z2 y2" + '\n' +
+                                                     "vertex x3 z3 y3" + '\n' +
+                                                 "endloop" + '\n' + "endfacet") + '\n' +
+                         
                                                 "endsolidmodel")
                 
 
     except:
         print('File creation error. Closing Python.')
         sys.exit(0)
-
-write()
+print current.ncols
+#write()
 
 
 print "Done!"
