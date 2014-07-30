53c78d583effc94d5f223ce6f072c553f22c2a53
Ferdia McKeogh <ferdiamckeogh@Ferdias-MacBook.local>
2014-07-30 11:27:36 +0100

    stlwrite.py now generates the base of the map.

--- STL Generation/stlwrite.py
+++ STL Generation/stlwrite.py
@@ -8,9 +8,8 @@ def write():
     name = 'MAPDATA.stl'
 
     try:
-            with open('MAPDATA.stl', 'a') as f2:
-#Convert map data to .stl format here, need the map data to start working.
-                f2.write("solid model\n" + file.read() +"\n" + "endsolidmodel")
+            with open('MAPDATA.stl', 'a') as f2:               
+                f2.write("solid model" + '\n' + ("facet normal 0.0 0.0 -1.0" + '\n' "outer loop" + '\n' + "vertex 100.0 0.0 0.0" + '\n' + "vertex 0.0 0.0 0.0" + '\n' + "vertex 0.0 0.0 100.0" + '\n' + "endloop" + '\n' + "endfacet") + '\n' + "endsolidmodel")
 
     except:
         print('File creation error. Closing Python.')
