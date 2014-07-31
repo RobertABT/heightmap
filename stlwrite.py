916503e77095ff56c3ab1fdd1dd188e2e74d6596
RobertABT <rbterrey@gmail.com>
2014-07-31 15:10:18 +0100

    updated stlwrite.py with usrheight

--- stlwrite.py
+++ stlwrite.py
@@ -6,14 +6,14 @@ import region
 from pylab import imread
 from scipy.ndimage import gaussian_filter
 from stl_tools import numpy2stl
+print 'Format required is as HP40 not hp40'
 usrselectedcoords = raw_input("Please enter desired Ordnance Survey map reference to be used: ")
-usrselectedcoords = usrselectedcoords.replace(" ","")
-usrselectedcoords = usrselectedcoords.upper()
+usrselectedheight = raw_input('Choose scale to divide height by (5 is a good start point)') 
 
 r = region.Region()
 r.readgr (usrselectedcoords)
 
 print "Generating STL file from map data..."
-filename = str('GENERATED_' + usrselectedcoords + '.stl')
-numpy2stl(r.grid, 'generatedstl/' + filename, solid=True)
+filename = str('GENERATED_' + usrselectedcoords +'_scale_' + usrselectedheight + '.stl')
+numpy2stl((r.grid / int(usrselectedheight)), 'generatedstl/' + filename, solid=True)
 print 'Done! ' + filename + ' is now ready to print!'
